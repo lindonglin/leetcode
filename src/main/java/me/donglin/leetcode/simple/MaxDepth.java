b913eabc175c459564c403d70623c82346cbfe51
lindonglin <lindonglin@ingbaobei.com>
2020-07-28 08:56:17 +0800

    104. 二叉树的最大深度

---
+++ src/main/java/me/donglin/leetcode/simple/MaxDepth.java
@@ -0,0 +1,51 @@
+package me.donglin.leetcode.simple;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 104. 二叉树的最大深度
+ * 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ *
+ * @author donglin
+ * @since 2020/07/28
+ */
+public class MaxDepth {
+
+    @Test
+    public void case1() {
+        Assert.assertEquals(1, maxDepth(new TreeNode(1)));
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
