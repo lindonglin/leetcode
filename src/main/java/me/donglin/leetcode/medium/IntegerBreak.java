a6feff0a55f07f6ca16ae52e446dbd2b9a76e5c0
lindonglin <lindonglin@ingbaobei.com>
2020-07-30 09:10:27 +0800

    343. 整数拆分

---
+++ src/main/java/me/donglin/leetcode/medium/IntegerBreak.java
@@ -0,0 +1,68 @@
+package me.donglin.leetcode.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * 343. 整数拆分
+ * 给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
+ *
+ * 示例 1:
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1。
+ *
+ * 示例 2:
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
+ * 说明: 你可以假设 n 不小于 2 且不大于 58。
+ *
+ * @author donglin
+ * @since 2020/07/30
+ */
+public class IntegerBreak {
+
+    private final int[] cache = new int[60];
+
+    @Test
+    public void case1() {
+        Assert.assertEquals(1, integerBreak(2));
+    }
+
+    @Test
+    public void case2() {
+        Assert.assertEquals(36, integerBreak(10));
+    }
+
+    @Test
+    public void case3() {
+        Assert.assertEquals(9, integerBreak(6));
+    }
+
+    @Test
+    public void case4() {
+        Assert.assertEquals(2, integerBreak(3));
+    }
+
+
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        if (cache[n] > 0) {
+            return cache[n];
+        }
+        cache[0] = cache[1] = 1;
+        cache[2] = 2;
+        for (int i = 3, size = cache.length; i < size; i++) {
+            int max = 0;
+            for (int k = 1; k <= i; k++) {
+                max = Math.max(max, k * cache[i - k]);
+            }
+            cache[i] = max;
+        }
+        return cache[n];
+    }
+}
