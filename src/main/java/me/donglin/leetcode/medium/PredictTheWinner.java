bd3423e206cf3cfd2dcab2d6fa4efb24f55b9c4a
lindonglin <1123941135@qq.com>
2020-09-02 00:03:56 +0800

    486. 预测赢家

---
+++ src/main/java/me/donglin/leetcode/medium/PredictTheWinner.java
@@ -0,0 +1,75 @@
+package me.donglin.leetcode.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 486. 预测赢家
+ * 给定一个表示分数的非负整数数组。 玩家 1 从数组任意一端拿取一个分数，随后玩家 2 继续从剩余数组任意一端拿取分数，然后玩家 1 拿，…… 。
+ * 每次一个玩家只能拿取一个分数，分数被拿取之后不再可取。直到没有剩余分数可取时游戏结束。最终获得分数总和最多的玩家获胜。
+ *
+ * 给定一个表示分数的数组，预测玩家1是否会成为赢家。你可以假设每个玩家的玩法都会使他的分数最大化。
+ *
+ * 示例 1：
+ * 输入：[1, 5, 2]
+ * 输出：False
+ * 解释：一开始，玩家1可以从1和2中进行选择。
+ * 如果他选择 2（或者 1 ），那么玩家 2 可以从 1（或者 2 ）和 5 中进行选择。如果玩家 2 选择了 5 ，那么玩家 1 则只剩下 1（或者 2 ）可选。
+ * 所以，玩家 1 的最终分数为 1 + 2 = 3，而玩家 2 为 5 。
+ * 因此，玩家 1 永远不会成为赢家，返回 False 。
+ *
+ * 示例 2：
+ * 输入：[1, 5, 233, 7]
+ * 输出：True
+ * 解释：玩家 1 一开始选择 1 。然后玩家 2 必须从 5 和 7 中进行选择。无论玩家 2 选择了哪个，玩家 1 都可以选择 233 。
+ *      最终，玩家 1（234 分）比玩家 2（12 分）获得更多的分数，所以返回 True，表示玩家 1 可以成为赢家。
+ *
+ * 提示：
+ *
+ * 1 <= 给定的数组长度 <= 20.
+ * 数组里所有分数都为非负数且不会大于 10000000 。
+ * 如果最终两个玩家的分数相等，那么玩家 1 仍为赢家。
+ *
+ * @author donglin
+ * @since 2020-09-01
+ */
+public class PredictTheWinner {
+
+    @Test
+    public void case1() {
+        Assert.assertFalse(predictTheWinner(new int[]{1, 5, 2}));
+    }
+
+    @Test
+    public void case2() {
+        Assert.assertTrue(predictTheWinner(new int[]{1, 5, 233, 7}));
+    }
+
+
+    @Test
+    public void case3() {
+        Assert.assertTrue(predictTheWinner(new int[]{1, 567, 1, 1}));
+    }
+
+    @Test
+    public void case4() {
+        Assert.assertFalse(predictTheWinner(new int[]{2, 4, 3, 4, 55, 1, 2, 3, 1, 2, 4, 3, 4, 5, 1}));
+    }
+
+
+    public boolean predictTheWinner(int[] nums) {
+        int length = nums.length;
+        // 分数差的最大值
+        int[][] dp = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][length - 1] >= 0;
+    }
+
+}
