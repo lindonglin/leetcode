51c0c4bebf1690d76f478d1d0c076071bc6de44c
lindonglin <1123941135@qq.com>
2020-09-23 21:41:06 +0800

    617. 合并二叉树

---
+++ src/main/java/me/donglin/leetcode/simple/MergeTrees.java
@@ -0,0 +1,56 @@
+package me.donglin.leetcode.simple;
+
+import me.donglin.leetcode.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 617. 合并二叉树
+ * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+ *
+ * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * 	Tree 1                     Tree 2
+ *           1                         2
+ *          / \                       / \
+ *         3   2                     1   3
+ *        /                           \   \
+ *       5                             4   7
+ * 输出:
+ * 合并后的树:
+ * 	     3
+ * 	    / \
+ * 	   4   5
+ * 	  / \   \
+ * 	 5   4   7
+ * 注意: 合并必须从两个树的根节点开始。
+ *
+ * @author donglin
+ * @since 2020-09-23
+ */
+public class MergeTrees {
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        TreeNode node = new TreeNode(t1.val + t2.val);
+        node.left = mergeTrees(t1.left, t2.left);
+        node.right = mergeTrees(t1.right, t2.right);
+        return node;
+    }
+
+    @Test
+    public void case1() {
+        TreeNode t1 = TreeNode.fullOrder(1, 3, 2, 5);
+        TreeNode t2 = TreeNode.fullOrder(2, 1, 3, null, 4, 7);
+        TreeNode expected = TreeNode.fullOrder(3, 4, 5, 5, 4, 7);
+        Assert.assertEquals(expected, mergeTrees(t1, t2));
+    }
+}
