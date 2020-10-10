cfb71852a4033c7a5a96b2d914fe5760e8fd6518
lindonglin <lindonglin@ingbaobei.com>
2020-10-10 08:58:18 +0800

    142. 环形链表 II

---
+++ src/main/java/me/donglin/leetcode/medium/DetectCycle.java
@@ -0,0 +1,76 @@
+package me.donglin.leetcode.medium;
+
+import me.donglin.leetcode.ListNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 142. 环形链表 II
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ *
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 如果 pos 是 -1，则在该链表中没有环。
+ *
+ * 说明：不允许修改给定的链表。
+ *
+ * 示例 1：
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：tail connects to node index 1
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ * 示例 2：
+ * 输入：head = [1,2], pos = 0
+ * 输出：tail connects to node index 0
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ *
+ * 示例 3：
+ * 输入：head = [1], pos = -1
+ * 输出：no cycle
+ * 解释：链表中没有环。
+ *
+ * 进阶：
+ * 你是否可以不用额外空间解决此题？
+ *
+ * @author donglin
+ * @since 2020/10/10
+ */
+public class DetectCycle {
+
+    public ListNode detectCycle(ListNode head) {
+        Set<Integer> cache = new HashSet<>();
+        ListNode node = head;
+        while (node != null) {
+            if (cache.contains(node.hashCode())) {
+                return node;
+            }
+            cache.add(node.hashCode());
+            node = node.next;
+        }
+        return null;
+    }
+
+
+    @Test
+    public void case1() {
+        ListNode head = ListNode.getInstance(3, 2, 0, -4);
+        head.next.next.next.next = head.next;
+        ListNode expected = head.next;
+        Assert.assertEquals(expected, detectCycle(head));
+    }
+
+    @Test
+    public void case2() {
+        ListNode head = ListNode.getInstance(1, 2);
+        head.next = head;
+        Assert.assertEquals(head, detectCycle(head));
+    }
+
+    @Test
+    public void case3() {
+        ListNode head = ListNode.getInstance(1);
+        Assert.assertNull(detectCycle(head));
+    }
+}
