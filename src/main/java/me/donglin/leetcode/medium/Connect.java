7d8e3adbe4fe25a8737b8904848c4839b3372133
lindonglin <1123941135@qq.com>
2020-10-15 20:33:29 +0800

    116. 填充每个节点的下一个右侧节点指针

--- src/main/java/me/donglin/leetcode/medium/Connect.java
+++ src/main/java/me/donglin/leetcode/medium/Connect.java
@@ -4,12 +4,9 @@ import me.donglin.leetcode.Node;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * 117. 填充每个节点的下一个右侧节点指针 II
- * 给定一个二叉树
+ * 116. 填充每个节点的下一个右侧节点指针
+ * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
  *
  * struct Node {
  *   int val;
@@ -18,26 +15,18 @@ import java.util.List;
  *   Node *next;
  * }
  * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
- *
  * 初始状态下，所有 next 指针都被设置为 NULL。
- *
- * 进阶：
- * 你只能使用常量级额外空间。
- * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
  *
  *
  * 示例：
- * 输入：root = [1,2,3,4,5,null,7]
- * 输出：[1,#,2,3,#,4,5,7,#]
+ * 输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+ *
+ * 输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
+ *
  * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
  *
- * 提示：
- * 树中的节点数小于 6000
- * -100 <= node.val <= 100
- *
- *
  * @author donglin
- * @since 2020-09-28
+ * @since 2020-10-15
  */
 public class Connect {
 
@@ -45,55 +34,29 @@ public class Connect {
         if (root == null) {
             return null;
         }
-        List<Node> nodes = new ArrayList<>();
-        if (root.left != null) {
-            nodes.add(root.left);
+        Node node = root, first;
+        while (node != null) {
+            first = node;
+            while (node != null && node.left != null) {
+                node.left.next = node.right;
+                if (node.next != null) {
+                    node.right.next = node.next.left;
+                }
+                node = node.next;
+            }
+            node = first.left;
         }
-        if (root.right != null) {
-            nodes.add(root.right);
-        }
-        connect(nodes);
         return root;
-    }
-
-    public void connect(List<Node> nodes) {
-        if (nodes.isEmpty()) {
-            return;
-        }
-        nodes.add(null);
-        List<Node> newNodes = new ArrayList<>();
-        Node node;
-        for (int i = 0; i < nodes.size() - 1; i++) {
-            node = nodes.get(i);
-            node.next = nodes.get(i + 1);
-            if (node.left != null) {
-                newNodes.add(node.left);
-            }
-            if (node.right != null) {
-                newNodes.add(node.right);
-            }
-        }
-        connect(newNodes);
     }
 
     @Test
     public void case1() {
-        Node root = Node.from(1, 2, 3, 4, 5, null, 7);
-        Node expected = Node.from(1, 2, 3, 4, 5, null, 7);
+        Node root = Node.from(1, 2, 3, 4, 5, 6, 7);
+        Node expected = Node.from(1, 2, 3, 4, 5, 6, 7);
         expected.left.next = expected.right;
         expected.left.left.next = expected.left.right;
-        expected.left.right.next = expected.right.right;
-        Assert.assertEquals(expected, connect(root));
-    }
-
-    @Test
-    public void case2() {
-        Node root = Node.from(1, 2, 3, 4, 5, null, 6, 7, null, null, null, null, 8);
-        Node expected = Node.from(1, 2, 3, 4, 5, null, 6, 7, null, null, null, null, 8);
-        expected.left.next = expected.right;
-        expected.left.left.next = expected.left.right;
-        expected.left.right.next = expected.right.right;
-        expected.left.left.left.next = expected.right.right.right;
+        expected.left.right.next = expected.right.left;
+        expected.right.left.next = expected.right.right;
         Assert.assertEquals(expected, connect(root));
     }
 
