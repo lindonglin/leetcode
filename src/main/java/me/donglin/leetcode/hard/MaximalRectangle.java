0655fac0702e10da94a93a24595f5ae34dbc6fe3
lindonglin <linge941231@gmail.com>
2020-12-26 21:27:48 +0800

    85. 最大矩形

---
+++ src/main/java/me/donglin/leetcode/hard/MaximalRectangle.java
@@ -0,0 +1,106 @@
+package me.donglin.leetcode.hard;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * 85. 最大矩形
+ * 给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ *
+ * 示例 1：
+ * 输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * 输出：6
+ * 解释：最大矩形如上图所示。
+ *
+ * 示例 2：
+ * 输入：matrix = []
+ * 输出：0
+ *
+ * 示例 3：
+ * 输入：matrix = [["0"]]
+ * 输出：0
+ *
+ * 示例 4：
+ * 输入：matrix = [["1"]]
+ * 输出：1
+ *
+ * 示例 5：
+ * 输入：matrix = [["0","0"]]
+ * 输出：0
+ *
+ * 提示：
+ * rows == matrix.length
+ * cols == matrix[0].length
+ * 0 <= row, cols <= 200
+ * matrix[i][j] 为 '0' 或 '1'
+ *
+ * @author donglin
+ * @since 2020-12-26
+ */
+public class MaximalRectangle {
+
+    public int maximalRectangle(char[][] matrix) {
+        int m = matrix.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = matrix[0].length;
+        int[][] left = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        int ret = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '0') {
+                    continue;
+                }
+                int width = left[i][j];
+                int area = width;
+                for (int k = i - 1; k >= 0; k--) {
+                    width = Math.min(width, left[k][j]);
+                    area = Math.max(area, (i - k + 1) * width);
+                }
+                ret = Math.max(ret, area);
+            }
+        }
+        return ret;
+    }
+
+    @Test
+    public void case1() {
+        char[][] matrix = {{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
+        Assert.assertEquals(6, maximalRectangle(matrix));
+    }
+
+    @Test
+    public void case2() {
+        char[][] matrix = {};
+        Assert.assertEquals(0, maximalRectangle(matrix));
+    }
+
+    @Test
+    public void case3() {
+        char[][] matrix = {{'0'}};
+        Assert.assertEquals(0, maximalRectangle(matrix));
+    }
+    @Test
+    public void case4() {
+        char[][] matrix = {{'1'}};
+        Assert.assertEquals(1, maximalRectangle(matrix));
+    }
+
+    @Test
+    public void case5() {
+        char[][] matrix = {{'0', '0'}};
+        Assert.assertEquals(0, maximalRectangle(matrix));
+    }
+
+}
