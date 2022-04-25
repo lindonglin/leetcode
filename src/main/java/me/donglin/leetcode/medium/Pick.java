ce8ba6625528c8b8e877312c7dcc95269bed3b01
lindonglin <linge941231@gmail.com>
2022-04-25 15:40:19 +0800

    398. 随机数索引

---
+++ src/main/java/me/donglin/leetcode/medium/Pick.java
@@ -0,0 +1,29 @@
+package me.donglin.leetcode.medium;
+
+import java.util.*;
+
+/**
+ * 398 随机数索引
+ * @author donglin
+ * @since 2022-04-25
+ */
+public class Pick {
+
+    private final Map<Integer, List<Integer>> map;
+
+    public Pick(int[] nums) {
+        map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], new ArrayList<>());
+            }
+            map.get(nums[i]).add(i);
+        }
+    }
+
+    public int pick(int target) {
+        List<Integer> list = map.get(target);
+        return list.get(new Random().nextInt(list.size()));
+    }
+
+}
