a6a1669a06eb28f02fafd6653644cd98983336e4
lindonglin <linge941231@gmail.com>
2022-05-15 11:35:14 +0800

    812. 最大三角形面积

---
+++ src/main/java/me/donglin/leetcode/simple/largestTriangleArea.java
@@ -0,0 +1,49 @@
+package me.donglin.leetcode.simple;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 812. 最大三角形面积
+ * 给定包含多个点的集合，从其中取三个点组成三角形，返回能组成的最大三角形的面积。
+ *
+ * 示例:
+ * 输入: points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+ * 输出: 2
+ * 解释:
+ * 这五个点如下图所示。组成的橙色三角形是最大的，面积为2。
+ *
+ * 注意:
+ * 3 <= points.length <= 50.
+ * 不存在重复的点。
+ *  -50 <= points[i][j] <= 50.
+ * 结果误差值在 10^-6 以内都认为是正确答案。
+ *
+ * @author donglin
+ * @since 2022-05-15
+ */
+public class largestTriangleArea {
+
+    public double largestTriangleArea(int[][] points) {
+        int n = points.length;
+        double ret = 0.0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    ret = Math.max(ret, triangleArea(points[i][0], points[i][1], points[j][0], points[j][1], points[k][0], points[k][1]));
+                }
+            }
+        }
+        return ret;
+    }
+
+    public double triangleArea(int x1, int y1, int x2, int y2, int x3, int y3) {
+        return 0.5 * Math.abs(x1 * y2 + x2 * y3 + x3 * y1 - x1 * y3 - x2 * y1 - x3 * y2);
+    }
+
+    @Test
+    public void case1() {
+        int[][] points = {{0, 0}, {0, 1}, {1, 0}, {0, 2}, {2, 0}};
+        Assert.assertEquals(2.0, largestTriangleArea(points), 0.000001);
+    }
+}
