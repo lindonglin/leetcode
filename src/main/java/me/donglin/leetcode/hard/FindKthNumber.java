5f6e8001822b29d72f01c15bfe64638d8576584d
lindonglin <linge941231@gmail.com>
2022-05-18 21:44:59 +0800

    668. 乘法表中第k小的数

---
+++ src/main/java/me/donglin/leetcode/hard/FindKthNumber.java
@@ -0,0 +1,25 @@
+package me.donglin.leetcode.hard;
+
+/**
+ * @author donglin
+ * @since 2022-05-18
+ */
+public class FindKthNumber {
+
+    public int findKthNumber(int m, int n, int k) {
+        int left = 1, right = m * n;
+        while (left < right) {
+            int x = left + (right - left) / 2;
+            int count = x / n * n;
+            for (int i = x / n + 1; i <= m; ++i) {
+                count += x / i;
+            }
+            if (count >= k) {
+                right = x;
+            } else {
+                left = x + 1;
+            }
+        }
+        return left;
+    }
+}
