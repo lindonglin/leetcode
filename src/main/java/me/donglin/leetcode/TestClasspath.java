23b7ce0d67c2319ae50cd478c9412ffea6f313c3
lindonglin <lindonglin@itiger.com>
2023-03-21 14:50:26 +0800

    delete profiles.xml

---
+++ src/main/java/me/donglin/leetcode/TestClasspath.java
@@ -0,0 +1,13 @@
+package me.donglin.leetcode;
+
+/**
+ * @author lindonglin
+ * @since 2023/3/21
+ */
+public class TestClasspath {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.class.path"));
+    }
+
+}
